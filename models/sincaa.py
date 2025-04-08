2ae6bdb754f2e8ccfbb9ad5d3fd8b6982ed4a8ab
Zoesgithub <xuc0d@vsc509-03-l.ibex.kaust.edu.sa>
2025-04-08 14:47:40 +0300

    update model

--- models/sincaa.py
+++ models/sincaa.py
@@ -42,7 +42,7 @@ class SinCAA(nn.Module):
         else:
             self.topological_net=construct_gps(args.topological_net_layers, args.model_channels, num_head=args.num_head, attn_type="multihead", norm=args.norm)
             self.model="GPS"
-        self.recover_info_convnet=gnn.models.GIN(args.model_channels, args.model_channels, 1)
+        #self.recover_info_convnet=gnn.models.GIN(args.model_channels, args.model_channels, 1)
         
         self.node_int_embeder = nn.ModuleList([nn.Embedding(
             100, args.model_channels) for _ in range(3)])
@@ -98,6 +98,7 @@ class SinCAA(nn.Module):
         assert edge_index.max()==x.shape[0]-1
         assert edge_index.shape[-1]==0 or edge_index.max()<len(x), f"{edge_index.max} {x.shape}"
         xs=[]
+        recovery_info_loss=0
         if self.model=="GAT":
             x=self.topological_net(x, edge_index,  edge_attr=edge_emb,batch=node_residue_index)
 
@@ -109,22 +110,17 @@ class SinCAA(nn.Module):
                     mask=torch.zeros_like(node_emb[:, :1])+1
                 x=x*mask
                 x = conv(x, edge_index, edge_attr=edge_emb,batch=node_residue_index)
+                recovery_info=self.recovery_info(x[mask.squeeze(-1)<1]).reshape(-1, 2, 100).reshape(-1, 100)
+                l=feats["nodes_int_feats"][..., :2][mask.squeeze(-1)<1].reshape(-1)
+                recovery_info_loss=recovery_info_loss+(nn.functional.cross_entropy(recovery_info, l)).mean()
                 xs.append(x)
         x=self.transform_layer(torch.cat(xs, -1))
         ret_emb=torch.scatter_reduce(x.new_zeros(node_residue_index.max()+1, x.shape[-1]), 0, node_residue_index[..., None].expand_as(x), x, include_self=False, reduce="mean")
         if self.emb_layer is not None:
             ret_emb=self.emb_layer(ret_emb)
-        #recovery_info=self.recovery_info(self.recover_info_convnet(ret_emb[node_residue_index], edge_index,batch=node_residue_index)).reshape(-1, 2, 100).reshape(-1, 100)
-        #l=feats["nodes_int_feats"][..., :2].reshape(-1)
-        recovery_info_loss=0#(nn.functional.cross_entropy(recovery_info, l)).mean()
-        
-        for i in range(3):
-            dmask=(torch.rand_like(node_emb[:, :1])<1-self.feat_dropout_rate).float()
-            x_rep=x*dmask
-            x_rep=self.recover_info_convnet(x_rep, edge_index,batch=node_residue_index)
-            recovery_info=self.recovery_info(x_rep[dmask.squeeze(-1)<1]).reshape(-1, 2, 100).reshape(-1, 100)
-            l=feats["nodes_int_feats"][..., :2][dmask.squeeze(-1)<1].reshape(-1)
-            recovery_info_loss=recovery_info_loss+(nn.functional.cross_entropy(recovery_info, l)).mean()
+        recovery_info=self.recovery_info(x).reshape(-1, 2, 100).reshape(-1, 100)
+        l=feats["nodes_int_feats"][..., :2].reshape(-1)
+        recovery_info_loss=recovery_info_loss+(nn.functional.cross_entropy(recovery_info, l)).mean()
         return x, ret_emb, recovery_info_loss, None
     
 
