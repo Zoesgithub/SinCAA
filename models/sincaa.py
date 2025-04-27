e07901efc4c667bf1adf86fd01db63a894aca4f1
Zoesgithub <xuc0d@vsc509-03-l.ibex.kaust.edu.sa>
2025-04-27 11:21:18 +0300

    update model

--- models/sincaa.py
+++ models/sincaa.py
@@ -209,11 +209,11 @@ class SinCAA(nn.Module):
         _, aa_pseudo_emb, rec_loss_aa, aa_acc= self.calculate_topol_emb(aa_data, add_mask=True)
         if self.aba==1: # use both zinc and aa
             return aa_pseudo_emb, aa_pseudo_emb, (rec_loss_mol+rec_loss_aa)/2, rec_loss_aa.new_zeros(len(aa_pseudo_emb)), (mol_acc).item(), rec_loss_aa.new_zeros(len(aa_pseudo_emb))
-        _, neighbor_pseudo_emb, _, _= self.calculate_topol_emb(neighbor_data, add_mask=True)
+        _, neighbor_pseudo_emb, rec_loss_nei, _= self.calculate_topol_emb(neighbor_data, add_mask=True)
         #_, aa_pseudo_emb, _, _= self.calculate_topol_emb(aa_data, add_mask=False)
         contract_pos=self.out_contrast(torch.cat([aa_pseudo_emb, neighbor_pseudo_emb], -1)).squeeze(-1)
         expand_shape=(aa_pseudo_emb.shape[0], neighbor_pseudo_emb.shape[0], neighbor_pseudo_emb.shape[-1])
         contract_neg=self.out_contrast(torch.cat([aa_pseudo_emb[:, None].expand(expand_shape), aa_pseudo_emb[None].expand(expand_shape)], -1)).squeeze(-1)
         contract_neg=contract_neg[torch.eye(contract_neg.shape[0]).to(contract_neg.device)==0]
         contrast_loss=torch.log(contract_pos.clamp(1e-8)).mean()+torch.log((1-contract_neg).clamp(1e-8)).mean()
-        return aa_pseudo_emb,neighbor_pseudo_emb, (rec_loss_mol+rec_loss_aa)/2, self.out_similarity(torch.cat([aa_pseudo_emb, neighbor_pseudo_emb], -1)).squeeze(-1), (mol_acc).item(), -contrast_loss
+        return aa_pseudo_emb,neighbor_pseudo_emb, (rec_loss_mol+rec_loss_aa+rec_loss_nei)/3, self.out_similarity(torch.cat([aa_pseudo_emb, neighbor_pseudo_emb], -1)).squeeze(-1), (mol_acc).item(), -contrast_loss
